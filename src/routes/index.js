40c7306f96d3dbf5c040c6d4e83d6f717ad55bd2
Gabriel <martinezgabrielalejandro4@gmail.com>
2023-08-01 00:29:02 -0300

    advances

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const routerUser = require('./user.route');
 const router = express.Router();
 
 // colocar las rutas aquí
-router.use("/users", routerUser)
 
 
 module.exports = router;
